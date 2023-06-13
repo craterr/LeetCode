29fa2fe8a192affa910d4e165f05da5e4f490983
craterr <106965125+craterr@users.noreply.github.com>
2023-06-13 11:27:49 +0530

    Time: 3 ms (40.59%), Space: 6 MB (41.72%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,27 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize)
+{
+int i = digitsSize - 1;
+    while (i >= 0) 
+    {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+        } else {
+            digits[i] += 1;
+            *returnSize = digitsSize;
+            return digits;
+        }
+        i--;
+    }
+    
+    int* result = (int*)malloc((digitsSize + 1) * sizeof(int));
+    result[0] = 1;
+    for (int j = 1; j < digitsSize + 1; j++) {
+        result[j] = digits[j - 1];
+    }
+    
+    *returnSize = digitsSize + 1;
+    return result;
+}
