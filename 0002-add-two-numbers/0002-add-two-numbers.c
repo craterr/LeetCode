bde0337252459d25abf088da8067bb9d28f19e26
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2023-11-09 22:53:35 +0530

    Time: 4 ms (97.41%), Space: 8.5 MB (47.59%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -5,48 +5,74 @@
  *     struct ListNode *next;
  * };
  */
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
-{
-    struct ListNode* new=(struct ListNode*)malloc(sizeof(struct ListNode));
-    struct ListNode* temp=new;
-    // temp->next=0;
-    // temp->val=val;
-    int v1,v2,result;
-    int carry=0;
+// struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+// {
+//     struct ListNode* new=(struct ListNode*)malloc(sizeof(struct ListNode));
+//     struct ListNode* temp=new;
+//     int v1,v2,result;
+//     int carry=0;
     
-    while (l1 || l2 || carry)
-     {
-        if(l1)
-        v1=l1->val;
-        else 
-            v1=0;
+//     while (l1 || l2 || carry)
+//      {
+//         if(l1)
+//         v1=l1->val;
+//         else 
+//             v1=0;
         
-        if(l2)
-        v2=l2->val; 
-        else 
-            v2=0;
+//         if(l2)
+//         v2=l2->val; 
+//         else 
+//             v2=0;
         
-        result=v1+v2+carry;
-        carry=result / 10;
-        result=result%10;
+//         result=v1+v2+carry;
+//         carry=result / 10;
+//         result=result%10;
         
-        struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->val = result;
-        newNode->next = NULL;
+//         struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+//         newNode->val = result;
+//         newNode->next = NULL;
         
-        temp->next=newNode;
+//         temp->next=newNode;
         
-        temp=temp->next;
-        if(l1)
-        l1=l1->next;
+//         temp=temp->next;
+//         if(l1)
+//         l1=l1->next;
       
         
-        if(l2)
-        l2=l2->next; 
+//         if(l2)
+//         l2=l2->next; 
         
         
 
-     }
-    return new->next;
+//      }
+//     return new->next;
 
+// }
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    struct ListNode *t1 = l1, *t2 = l2;
+    struct ListNode *head=NULL, *t3=NULL;
+    int sum=0;
+    
+    while(t1 || t2 || sum){
+        if(t1){
+            sum += t1->val;
+            t1 = t1->next;
+        }if(t2){
+            sum += t2->val;
+            t2 = t2->next;
+        }
+        struct ListNode *new = malloc(sizeof(struct ListNode));
+        new->val = sum%10;
+        sum /= 10;
+        if(head){
+            t3->next = new;
+            t3 = new;
+        }else
+            head = t3 = new;
+    }
+    t3->next = NULL;
+    return head;
+    
 }
