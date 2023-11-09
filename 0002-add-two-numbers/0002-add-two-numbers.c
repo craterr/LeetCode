3d272476853b1abe4227eb8d27fb6dac0d5f08ed
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2023-11-09 23:17:05 +0530

    Time: 4 ms (97.41%), Space: 8.6 MB (8.20%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.c
+++ 0002-add-two-numbers/0002-add-two-numbers.c
@@ -5,57 +5,19 @@
  *     struct ListNode *next;
  * };
  */
-// struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
-// {
-//     struct ListNode* new=(struct ListNode*)malloc(sizeof(struct ListNode));
-//     struct ListNode* temp=new;
-//     int v1,v2,result;
-//     int carry=0;
-    
-//     while (l1 || l2 || carry)
-//      {
-//         if(l1)
-//         v1=l1->val;
-//         else 
-//             v1=0;
-        
-//         if(l2)
-//         v2=l2->val; 
-//         else 
-//             v2=0;
-        
-//         result=v1+v2+carry;
-//         carry=result / 10;
-//         result=result%10;
-        
-//         struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
-//         newNode->val = result;
-//         newNode->next = NULL;
-        
-//         temp->next=newNode;
-        
-//         temp=temp->next;
-//         if(l1)
-//         l1=l1->next;
-      
-        
-//         if(l2)
-//         l2=l2->next; 
-        
-        
-
-//      }
-//     return new->next;
-
-// }
 
 
-struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) 
+{
     struct ListNode *t1 = l1, *t2 = l2;
     struct ListNode *head=NULL, *t3=NULL;
+    // Create pointers t1 and t2 to traverse the input lists, and t3 and head for the result.
     int sum=0;
     
-    while(t1 || t2 || sum){
+    while(t1 || t2 || sum) 
+        // ('or sum' is used for a edge case when there should be a new node creation because of carry            like l1=4,l2=6 result should create a new node to carry ; [1,0] )
+    
+    {
         if(t1){
             sum += t1->val;
             t1 = t1->next;
@@ -63,10 +25,15 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
             sum += t2->val;
             t2 = t2->next;
         }
+        
+        
+        // Create a new node to store the ones digit and carry 
         struct ListNode *new = malloc(sizeof(struct ListNode));
-        new->val = sum%10;
-        sum /= 10;
-        if(head){
+        new->val = sum%10; // store the ones digit.
+        sum /= 10; //carry
+        
+        if(head)
+        {
             t3->next = new;
             t3 = new;
         }else
