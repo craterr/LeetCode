13581678ac10b97f3930746216131c35bd3bc1c7
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2023-11-09 23:43:09 +0530

    Time: 8 ms (16.67%), Space: 8.3 MB (16.67%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.c
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.c
@@ -7,6 +7,10 @@ int countHomogenous(char * s)
     long long int res=1;
     long long int mod=(long long int)pow(10,9)+7;
     
+    // this is often used in programming to prevent integer overflow.
+    //     Integer overflow occurs when the result of an arithmetic operation 
+    //     xceeds the maximum representable value for the data type being used.
+    
     for(int i=1;i<size;i++)
     {
         count=(s[i]==s[i-1]?count+1:1);
