83f394b9b9d97d934bf39c2c8dc7a2601f9ed8a3
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2023-11-13 20:34:43 +0530

    Time: 3 ms (51.57%), Space: 6.7 MB (17.02%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.c
@@ -0,0 +1,15 @@
+int rob(int* nums, int numsSize) {
+    if (numsSize == 0) {
+        return 0;
+    }
+    
+    int rob1 = 0, rob2 = 0;
+
+    for (int i = 0; i < numsSize; i++) {
+        int temp = rob1;
+        rob1 = fmax(rob2 + nums[i], rob1);
+        rob2 = temp;
+    }
+
+    return rob1;
+}
