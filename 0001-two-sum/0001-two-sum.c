f596c6f962a7034883156dcde66b47ee7cd8110f
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2024-01-01 22:16:28 +0530

    Time: 187 ms (6.09%), Space: 7 MB (78.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.c
+++ 0001-two-sum/0001-two-sum.c
@@ -1,26 +1,50 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+//int* twoSum(int* nums, int numsSize, int target, int* returnSize) 
+// {
+//     // int n=len(numsSize);
+//     for (int i=0;i<numsSize-1;i++)
+//     {
+//         for(int j=i+1;j<numsSize;j++)
+//         {
+//             if (nums[i]+nums[j]==target)
+//                 {
+//                     int*result=(int*)malloc(2*sizeof(int));
+//                     result[0]=i;
+//                     result[1]=j;
+                
+//                     *returnSize=2;
+//                     return result;
+//             }
+//         }
+//     }
+    
+//     return NULL;
+    
+    
+// }
+    
+    
+ int* twoSum(int* nums, int numsSize, int target, int* returnSize)    
 {
-    // int n=len(numsSize);
-    for (int i=0;i<numsSize-1;i++)
+    
+    for(int i=0;i<numsSize-1;i++)
     {
         for(int j=i+1;j<numsSize;j++)
         {
-            if (nums[i]+nums[j]==target)
-                {
-                    int*result=(int*)malloc(2*sizeof(int));
-                    result[0]=i;
-                    result[1]=j;
-                
-                    *returnSize=2;
-                    return result;
+            if(nums[i]+nums[j]==target)
+            {
+                int *result=malloc(2*sizeof(int));
+                result[0]=i;
+                result[1]=j;
+                *returnSize=2;
+                return result;
             }
         }
     }
-    
     return NULL;
     
+  
     
 }
