73ba392d431c2b69fe944d3391b8751c77c69609
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2024-01-10 00:52:44 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (46.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.c
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.c
@@ -1,15 +1,27 @@
-int minCostClimbingStairs(int* cost, int costSize){
-    int ans ;
-    int* stairs = (int*)malloc(costSize * sizeof(int));
-    stairs[0] = cost[0];
-    stairs[1] = cost[1];
-    
-    for(int i = 2; i < costSize; i++){
-        stairs[i] = (stairs[i-1] <= stairs[i-2]) ? (stairs[i-1] + cost[i]) : (stairs[i-2] + cost[i]);
+#include <limits.h>
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int minCostClimbingStairs(int* cost, int costSize) {
+    if (costSize <= 1) {
+        return 0;  
     }
     
-    ans = fmin(stairs[costSize-1], stairs[costSize-2]);
+    int* dp = (int*)malloc(costSize * sizeof(int));
+    dp[0] = cost[0];
+    dp[1] = cost[1];
+
     
-    free(stairs);
-    return ans;
+    for (int i = 2; i < costSize; i++) {
+        dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+    }
+
+    // The minimum cost to reach the top is the minimum of the last two steps
+    int result = min(dp[costSize - 1], dp[costSize - 2]);
+
+    free(dp);
+
+    return result;
 }
