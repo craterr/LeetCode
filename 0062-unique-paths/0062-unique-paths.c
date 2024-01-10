ae4ddca08c219a504e43fbc84bd9481bea362515
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2024-01-11 02:09:34 +0530

    Time: 3 ms (26.79%), Space: 6.5 MB (31.25%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.c
+++ 0062-unique-paths/0062-unique-paths.c
@@ -1,26 +1,22 @@
-#include <stdio.h>
-
-int  uniquePaths(int m, int n) {
-    if (m <= 0 || n <= 0) return 0;
-
+int uniquePaths(int m, int n) {
+    if(m<0 || n<0) return 0;
+    
     int dp[m][n];
-
-    // Initialize the first row and first column to 1
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < n; ++j) {
-            if (i == 0 || j == 0) {
-                dp[i][j] = 1;
-            }
+    
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            dp[i][j]=1;
         }
     }
-
-    // Fill the dp array using dynamic programming
-    for (int i = 1; i < m; ++i) {
-        for (int j = 1; j < n; ++j) {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    
+    for(int i=1;i<m;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            dp[i][j]=dp[i-1][j]+dp[i][j-1]; // at (1,1) we want to go either right(i-1,j) or down(i,j-1)
         }
     }
-
-    return dp[m - 1][n - 1];
+    return dp[m-1][n-1];
 }
-
