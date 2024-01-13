71cd36faa3dfab7298514db634bc65ef22a46f13
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2024-01-13 22:54:29 +0530

    Time: 7 ms (80.00%), Space: 7.8 MB (97.47%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.c
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -1,33 +1,33 @@
 int minPathSum(int** grid, int gridSize, int* gridColSize) 
 {
-    int m = gridSize;
-    int n = *gridColSize;
+    int m=gridSize;
+    int n=*gridColSize;
     int dp[m][n];
+    int top=0;
+    int left=0;
+    dp[0][0]=grid[0][0];
     
-    dp[0][0] = grid[0][0];
-    
-    // Initialize the first column
-    for(int i = 1; i < m; i++)
+    for(int i=1;i<m;i++)
     {
-        dp[i][0] = grid[i][0] + dp[i-1][0];
+        dp[i][0]=grid[i][0]+dp[i-1][0];
     }
     
-    // Initialize the first row
-    for(int j = 1; j < n; j++)
+     for(int j=1;j<n;j++)
     {
-        dp[0][j] = grid[0][j] + dp[0][j-1];
+        dp[0][j]=grid[0][j]+dp[0][j-1];
     }
     
-    // Build dp array
-    for(int i = 1; i < m; i++)
+    for(int i=1;i<m;i++)
     {
-        for(int j = 1; j < n; j++)
+        for(int j=1;j<n;j++)
         {
-            int top = grid[i][j] + dp[i][j-1];
-            int left = grid[i][j] + dp[i-1][j];
-            dp[i][j] = fmin(top, left);
+            top=grid[i][j]+dp[i][j-1];
+            left=grid[i][j]+dp[i-1][j];
+            dp[i][j]=fmin(top,left);
+            
+            
         }
     }
-    
     return dp[m-1][n-1];
+    
 }
