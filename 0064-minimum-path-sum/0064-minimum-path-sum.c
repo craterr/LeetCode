761a755f5f8d5598af9d86e8dad336045462f805
Kusum Manisha <106965125+craterr@users.noreply.github.com>
2024-01-14 12:23:18 +0530

    Time: 10 ms (48.54%), Space: 7.8 MB (84.63%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.c
+++ 0064-minimum-path-sum/0064-minimum-path-sum.c
@@ -1,20 +1,20 @@
 int minPathSum(int** grid, int gridSize, int* gridColSize) 
 {
     int m=gridSize;
-    int n=*gridColSize;
+    int n=*gridColSize; // deferencing  the value pointed by gridColSize
     int dp[m][n];
     int top=0;
     int left=0;
-    dp[0][0]=grid[0][0];
+    dp[0][0]=grid[0][0]; // initialising the dp matrix's first cell
     
     for(int i=1;i<m;i++)
     {
-        dp[i][0]=grid[i][0]+dp[i-1][0];
+        dp[i][0]=grid[i][0]+dp[i-1][0]; // for the first row in dp matrix
     }
     
      for(int j=1;j<n;j++)
     {
-        dp[0][j]=grid[0][j]+dp[0][j-1];
+        dp[0][j]=grid[0][j]+dp[0][j-1]; // for the first column in dp matrix
     }
     
     for(int i=1;i<m;i++)
@@ -23,7 +23,7 @@ int minPathSum(int** grid, int gridSize, int* gridColSize)
         {
             top=grid[i][j]+dp[i][j-1];
             left=grid[i][j]+dp[i-1][j];
-            dp[i][j]=fmin(top,left);
+            dp[i][j]=fmin(top,left); // finding the minimum path
             
             
         }
